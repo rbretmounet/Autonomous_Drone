a9b67ae2372feeac2d22525097db7bd0606d6a3f
rbretmounet <rbretmounet@gmail.com>
2018-11-29 16:51:58 -0800

    Added main folder to open .ino file or main.

---
+++ Main/MotorControl.ino
@@ -0,0 +1,13 @@
+// ****************************************************************** 
+// This File will be used for basic motor control for basic movement.*
+// Such as yaw, roll, and pitch of the drone.                        *
+// *******************************************************************
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
