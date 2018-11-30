4813d6057725f712ede0af2207a5d3267e8e3bb0
rbretmounet <rbretmounet@gmail.com>
2018-11-29 17:10:13 -0800

    added variables functions and declerations for motor control.

--- Main/MotorControl.ino
+++ Main/MotorControl.ino
@@ -1,13 +1,33 @@
-// ****************************************************************** 
+// ******************************************************************* 
 // This File will be used for basic motor control for basic movement.*
-// Such as yaw, roll, and pitch of the drone.                        *
+// Such as yaw, roll, , position, and pitch of the drone.            *
 // *******************************************************************
-void setup() {
-  // put your setup code here, to run once:
+#include <Servo.h>
+// Function prototypes
+void upwardMovement(Servo &, Servo &, Servo &, Servo &, int);
 
+Servo motor_1; // Used to communicate values to the esc analog pin connected to motor 1.
+Servo motor_2; // Used to communicate values to the esc analog pin connected to motor 2.
+Servo motor_3; // Used to communicate values to the esc analog pin connected to motor 3.
+Servo motor_4; // Used to communicate values to the esc analog pin connected to motor 4.
+int throttle = 0; // Used to control the throttle speed of the motor.
+
+void setup() 
+{
+  motor_1.attach(9); // Declares the pin of the first esc. 
+  motor_2.attach(9); // Declares the pin of the second esc.
+  motor_3.attach(9); // Declares the pin of the third esc.
+  motor_4.attach(9); // Declares the pin of the fourth esc.  
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+void loop() 
+{
+  upwardMovement(motor_1,motor_2,motor_3,motor_4,throttle);
 
 }
+// Used to increase the drone's altitude.
+void upwardMovement (Servo &motor_1, Servo &motor_2, Servo &motor_3, Servo &motor_4, int throttle)
+{
+  
+}
+
