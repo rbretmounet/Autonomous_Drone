8e7b5b5f4a587953b98c002a9cec878cfab418ec
rbretmounet <rbretmounet@gmail.com>
2018-11-29 16:31:24 -0800

    Created Desctription of what each files function will be for the drone.

---
+++ GPS/GPS.ino
@@ -0,0 +1,13 @@
+// This file is used to get the coordinates of the current position
+// of the drone. This will be used to help the drone too know where
+// its current location is relative to its final destination.
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
---
+++ Gyroscope/Gyroscope.ino
@@ -0,0 +1,12 @@
+// This files is used to tell what the current orientation of the drone is.
+// It will be used to keep the drone level and to adjust the direction it
+// will face.
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
