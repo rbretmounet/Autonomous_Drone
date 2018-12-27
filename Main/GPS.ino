7e3c748a709807844afe00d9a2b3f127acd500d4
rbretmounet <rbretmounet@gmail.com>
2018-12-27 11:15:38 -0800

    added functions for setting the home destination of the gps, print out the data that the gps returns, getting the current position of the drone, calculating the distance from current position to the final destination and, a function to set the destination values.

--- Main/GPS.ino
+++ Main/GPS.ino
@@ -3,12 +3,55 @@
 // of the drone. This will be used to help the drone too know where*
 // its current location is relative to its final destination.      *
 // *****************************************************************
-void setup() {
-  // put your setup code here, to run once:
 
+// Used to set home position of drone
+void setHome()
+{
+  homeLattitude = (gps.location.lat());
+  homeLongitude = (gps.location.lng());
+  homeAltitude = (gps.altitude.meters());
+}
+// Used to print out all the gps data that is needed.
+void printData()
+{
+   while (gpsSerial.available())
+  {
+    int data = gpsSerial.read();
+    if (gps.encode(data))
+    {
+      Serial.print ("lattitude: ");
+      Serial.println (currentLattitude);
+      Serial.println("");
+      Serial.print ("longitude: ");
+      Serial.println (currentLongitude);
+      Serial.println("");
+      Serial.print ("altitude: ");
+      Serial.println(currentAltitude);
+      Serial.println("");
+      Serial.print ("distance: ");
+      Serial.println(Distance_To_Destination);
+    }
+  }
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+// Sets gps data to a variable that will be used to know the current location of the drone.
+void getData()
+{
+  currentLattitude = (gps.location.lat());
+  currentLongitude = (gps.location.lng());
+  currentAltitude = (gps.altitude.meters());
+}
 
+// Used to set the destination data.
+void setDestination(float altitude, float lattitude, float longitude)
+{
+  destinationAltitude = altitude;
+  destinationLongitude = longitude;
+  destinationLattitude = lattitude;
+}
+
+// Used to calculated the distance to the final destination
+void updateDistance(float desLongitude, float desLattitude, float currLongitude, float currLattitude)
+{
+  Distance_To_Destination = TinyGPSPlus::distanceBetween(desLattitude,desLongitude,currLattitude,currLongitude);
 }
