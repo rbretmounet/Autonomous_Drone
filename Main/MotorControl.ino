7c54b19a58265f9c99ef91e20f65d1bf65211d8c
rbretmounet <rbretmounet@gmail.com>
2018-12-26 22:19:26 -0800

    Added two different functions for increase in altitude and a decrease in alitude for each motor.

--- Main/MotorControl.ino
+++ Main/MotorControl.ino
@@ -3,31 +3,46 @@
 // Such as yaw, roll, , position, and pitch of the drone.            *
 // *******************************************************************
 #include <Servo.h>
-// Function prototypes
-void upwardMovement(Servo &, Servo &, Servo &, Servo &, int);
 
 Servo motor_1; // Used to communicate values to the esc analog pin connected to motor 1.
 Servo motor_2; // Used to communicate values to the esc analog pin connected to motor 2.
 Servo motor_3; // Used to communicate values to the esc analog pin connected to motor 3.
 Servo motor_4; // Used to communicate values to the esc analog pin connected to motor 4.
-int throttle = 0; // Used to control the throttle speed of the motor.
+int throttle = 900; // Used to control the throttle speed of the motor.
 
 void setup() 
 {
   motor_1.attach(9); // Declares the pin of the first esc. 
-  motor_2.attach(9); // Declares the pin of the second esc.
-  motor_3.attach(9); // Declares the pin of the third esc.
-  motor_4.attach(9); // Declares the pin of the fourth esc.  
+  motor_2.attach(10); // Declares the pin of the second esc.
+  motor_3.attach(11); // Declares the pin of the third esc.
+  motor_4.attach(12); // Declares the pin of the fourth esc.  
+  Serial.begin(9600);
 }
 
-void loop() 
-{
-  upwardMovement(motor_1,motor_2,motor_3,motor_4,throttle);
-
-}
 // Used to increase the drone's altitude.
-void upwardMovement (Servo &motor_1, Servo &motor_2, Servo &motor_3, Servo &motor_4, int throttle)
+void upwardMovement()
 {
-  
+  for(int i = 900; i < 2300; i += 1)  // goes from 0 degrees to 2300 degrees in steps of 1 degree  
+  {                         
+    throttle++;         
+    motor_1.writeMicroseconds(throttle); // tell motor to go to position in variable 'throttle' 
+    motor_2.writeMicroseconds(throttle); // tell motor to go to position in variable 'throttle' 
+    motor_3.writeMicroseconds(throttle); // tell motor to go to position in variable 'throttle' 
+    motor_4.writeMicroseconds(throttle); // tell motor to go to position in variable 'throttle' 
+    delay(15);       
+    Serial.println(throttle); // waits 15ms for the servo to reach the position 
+  }
 }
-
+void downwardMovement()
+{
+  for(int i = 2300; i > 900; i -= 1)  // goes from 2300 degrees to 0 degrees in steps of 1 degree  
+  {
+    throttle--;                                  
+    motor_1.writeMicroseconds(throttle); // tell motor to go to position in variable 'throttle' 
+    motor_2.writeMicroseconds(throttle); // tell motor to go to position in variable 'throttle' 
+    motor_3.writeMicroseconds(throttle); // tell motor to go to position in variable 'throttle' 
+    motor_4.writeMicroseconds(throttle); // tell motor to go to position in variable 'throttle' 
+    delay(15);                       
+    Serial.println(throttle);// waits 15ms for the servo to reach the position 
+  }
+}
