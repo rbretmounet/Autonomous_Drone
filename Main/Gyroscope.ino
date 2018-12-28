7c62f7640bcb7cad3eb76f47648762661b7739b8
rbretmounet <rbretmounet@gmail.com>
2018-12-27 18:19:25 -0800

    Added a function to print out angle values and a function to store the angle values.

--- Main/Gyroscope.ino
+++ Main/Gyroscope.ino
@@ -3,12 +3,20 @@
 // It will be used to keep the drone level and to adjust the direction it  *
 // will face.                                                              *
 // *************************************************************************
-void setup() {
-  // put your setup code here, to run once:
 
+void printGyroData()
+{
+  mpu6050.update();
+  Serial.print("angleX : ");
+  Serial.print(angleX);
+  Serial.print("\tangley : ");
+  Serial.print(angleY);
+  Serial.print("\tangleZ : ");
+  Serial.println(angleZ);
 }
-
-void loop() {
-  // put your main code here, to run repeatedly:
-
+void getGyroData()
+{
+  angleX = mpu6050.getAngleX();
+  angleY = mpu6050.getAngleY();
+  angleZ = mpu6050.getAngleZ();
 }
